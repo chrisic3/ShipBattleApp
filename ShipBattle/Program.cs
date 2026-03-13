607e4e69a2089a5d83f4ea6631c7e3fa29945bde
chrisic3 <chris_ic3@hotmail.com>
2026-03-13 15:55:34 -0500

    Add GameLogic and Begin Adding UI Methods

--- ShipBattle/Program.cs
+++ ShipBattle/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ShipBattleLibrary;
+using ShipBattleLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +12,51 @@ namespace ShipBattle
     {
         static void Main(string[] args)
         {
+            WelcomeMessage();
 
             Console.ReadLine();
         }
-    }
+
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Ship Battle, based on the game " +
+                "Battleship");
+            Console.WriteLine("Designed by Tim Corey");
+            Console.WriteLine("Built and modified by Chris Stelly\n");
+        }
+
+        private static PlayerInfoModel CreatePlayer()
+        {
+            PlayerInfoModel output = new PlayerInfoModel();
+
+            // Ask for player name
+            output.PlayerName = AskForPlayerName();
+
+            // Load the shot grid
+            GameLogic.InitializeGrid(output);
+
+            // Ask for ship locations
+            
+            
+            // Clear
+        }
+
+        private static string AskForPlayerName()
+        {
+            Console.Write("What is your name: ");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+        // This is here becase it needs UI
+        private static void PlaceShips(PlayerInfoModel player)
+        {
+            do
+            {
+                Console.Write($"Where do you want to place ship number " +
+                    $"{ player.PlayerShipLocations.Count + 1 }: ");
+                string location = Console.ReadLine();
+            } while (player.PlayerShipLocations.Count < 5); // May make dynamic
+        }
 }
---
+++ ShipBattleLibrary/GameLogic.cs
@@ -0,0 +1,64 @@
+﻿using ShipBattleLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipBattleLibrary
+{
+    public static class GameLogic
+    {
+        // This is here because it needs no UI
+        public static void InitializeGrid(PlayerInfoModel player)
+        {
+            /**
+             * This is supposed to be done in the Logic step
+             */
+
+            //List<string> letters = new List<string>
+            //{
+            //    "A",
+            //    "B",
+            //    "C",
+            //    "D",
+            //    "E"
+            //};
+
+            //List<int> numbers = new List<int>
+            //{
+            //    1,
+            //    2,
+            //    3,
+            //    4,
+            //    5
+            //};
+
+            //foreach (string letter in letters)
+            //{
+            //    foreach (int number in numbers)
+            //    {
+            //        AddGridSpot(player, letter, number);
+            //    }
+            //}
+        }
+
+        /**
+         * This is supposed to be done in the Logic step
+         */
+
+        //private static void AddGridSpot(PlayerInfoModel player, string letter,
+        //    int number)
+        //{
+        //    GridSpotModel spot = new GridSpotModel
+        //    {
+        //        SpotLetter = letter,
+        //        SpotNumber = number,
+        //        Status = GridSpotStatus.Empty
+        //    };
+
+        //    player.PlayerShots.Add(spot);
+        //}
+    }
+}
